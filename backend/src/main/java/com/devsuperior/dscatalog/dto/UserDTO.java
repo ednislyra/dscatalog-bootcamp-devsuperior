a7f6ef30409ff8817df9439d1f6b3d1ed1a589b8
Ednis Lyra <ednislyra@yahoo.com>
2021-06-15 20:30:42 +0100

    Basic Beans Validation

--- backend/src/main/java/com/devsuperior/dscatalog/dto/UserDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserDTO.java
@@ -4,15 +4,19 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
-import com.devsuperior.dscatalog.entities.Role;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 import com.devsuperior.dscatalog.entities.User;
 
 public class UserDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
+	@NotBlank(message = "Campo Obrigatório")
 	private String firstName;
 	private String lastName;
+	@Email(message = "Favor entrar um e-mail válido")
 	private String email;
 	
 	Set<RoleDTO> roles = new HashSet<>();
--- backend/src/main/java/com/devsuperior/dscatalog/resources/ProductResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/ProductResource.java
@@ -2,6 +2,8 @@ package com.devsuperior.dscatalog.resources;
 
 import java.net.URI;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -34,27 +36,27 @@ public class ProductResource {
 		return ResponseEntity.ok().body(list);
 	}
 	
-	@GetMapping(value = "/{id}") //Busca por Id
+	@GetMapping(value = "/{id}")
 	public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
 		ProductDTO dto = service.findById(id);		
 		return ResponseEntity.ok().body(dto);
 	}
 	
-	@PostMapping //Inserir
-	public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto){
+	@PostMapping
+	public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto){
 		dto = service.insert(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(dto.getId()).toUri();
 		return ResponseEntity.created(uri).body(dto);
 	}
 	
-	@PutMapping(value = "/{id}") //Atualizar
-	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto){
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @Valid @RequestBody ProductDTO dto){
 		dto = service.update(id, dto);
 		return ResponseEntity.ok().body(dto);
 	}
 	
-	@DeleteMapping(value = "/{id}") //Atualizar
+	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Long id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();

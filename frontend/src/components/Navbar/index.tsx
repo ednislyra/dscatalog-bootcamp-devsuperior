ec5e44da0f9b9b11402ed77fe41434b7035f60a9
Ednis Lyra <ednislyra@yahoo.com>
2021-09-30 21:47:38 +0100

    Frontend: Home Styling(Responsive)

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -23,9 +23,7 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="dscatalog-navbar">
           <ul className="navbar-nav offset-md-2 main-menu">
             <li>
-              <a href="link" className="active">
-                HOME
-              </a>
+              <a href="link" className="active">HOME</a>
             </li>
             <li>
               <a href="link">CATÁLOGO</a>

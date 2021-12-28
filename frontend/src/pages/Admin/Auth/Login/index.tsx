0741ff3c7b7b8fa7f9ba2fdf4ba45a58e03d0c2d
Ednis Lyra <ednislyra@yahoo.com>
2021-12-28 15:05:07 +0000

    Frontend: React-Hook-Form First Test

--- frontend/src/pages/Admin/Auth/Login/index.tsx
+++ frontend/src/pages/Admin/Auth/Login/index.tsx
@@ -1,14 +1,28 @@
 import { Link } from 'react-router-dom';
 import ButtonIcon from 'components/ButtonIcon';
 import './styles.css';
+import { useForm } from 'react-hook-form';
+
+type FormData = {
+    username: string;
+    password: string;
+
+}
 
 const Login = () => {
+
+    const { register, handleSubmit } = useForm<FormData>();
+    const onSubmit = (formData: FormData) => {
+        console.log(formData);
+    };
+
   return (
     <div className="base-card login-card">
       <h1>LOGIN</h1>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-4">
-          <input
+          <input 
+          {...register("username")}
             type="text"
             className="form-control base-input"
             placeholder="Email"
@@ -16,7 +30,8 @@ const Login = () => {
           />
         </div>
         <div className="mb-2">
-          <input
+          <input 
+          {...register("password")}
             type="password"
             className="form-control base-input "
             placeholder="Password"

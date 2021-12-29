8e7bc155b6e4ba63be0b4ec0e42048a7285e91da
Ednis Lyra <ednislyra@yahoo.com>
2021-12-29 12:42:39 +0000

    Frontend: Login Request

--- frontend/src/pages/Admin/Auth/Login/index.tsx
+++ frontend/src/pages/Admin/Auth/Login/index.tsx
@@ -1,28 +1,37 @@
 import { Link } from 'react-router-dom';
 import ButtonIcon from 'components/ButtonIcon';
-import './styles.css';
 import { useForm } from 'react-hook-form';
+import { requestBackendLogin } from 'util/requests';
+
+import './styles.css';
+
 
 type FormData = {
-    username: string;
-    password: string;
-
-}
+  username: string;
+  password: string;
+};
 
 const Login = () => {
+  const { register, handleSubmit } = useForm<FormData>();
 
-    const { register, handleSubmit } = useForm<FormData>();
-    const onSubmit = (formData: FormData) => {
-        console.log(formData);
-    };
+  const onSubmit = (formData : FormData) => {
+    requestBackendLogin(formData)
+      .then(response => {
+        console.log('SUCESSO', response);
+      })
+      .catch(error => {
+        console.log('ERRO', error);
+      });
+  };
+ 
 
   return (
     <div className="base-card login-card">
       <h1>LOGIN</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-4">
-          <input 
-          {...register("username")}
+          <input
+            {...register("username")}
             type="text"
             className="form-control base-input"
             placeholder="Email"
@@ -30,8 +39,8 @@ const Login = () => {
           />
         </div>
         <div className="mb-2">
-          <input 
-          {...register("password")}
+          <input
+            {...register("password")}
             type="password"
             className="form-control base-input "
             placeholder="Password"

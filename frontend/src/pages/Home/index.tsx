01001d6f992c7d614bbcd13343b09f9efd03ca31
Ednis Lyra <ednislyra@yahoo.com>
2021-12-31 13:53:14 +0000

    Frontend: PrivateRoute

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,16 +1,11 @@
 import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
 import ButtonIcon from 'components/ButtonIcon';
-import './styles.css';
 import { Link } from 'react-router-dom';
-import { getTokenData, isAuthenticated } from 'util/requests';
+import './styles.css';
 
 const Home = () => {
   return (
     <div className="home-container">
-
-      <h1>{getTokenData()?.user_name}</h1>
-      <h1>{isAuthenticated() ? 'autenticado' : 'não autenticado'}</h1>
-
       <div className="base-card home-card">
         <div className="home-content-container">
           <div>
